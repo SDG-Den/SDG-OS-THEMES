4a18eff832b9f56f2701c9db629cc959b6c84953
SDGDen <sdgden@sdgcloud.nl>
2026-07-09 22:50:16 +0200

    fixed pathing

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 
 WORKDIR=/home/$(whoami)/.cache/SDG-PKG/sdg-themes
 
+mkdir -p /home/$(whoami)/.config/SDG-THEMES
 cp -r $WORKDIR/config/SDG-THEMES/* /home/$(whoami)/.config/SDG-THEMES
-
+mkdir -p /home/$(whoami)/.local/SDG-THEMES
 cp -r $WORKDIR/local/SDG-THEMES/* /home/$(whoami)/.local/SDG-THEMES
 chmod a+x /home/$(whoami)/.local/SDG-THEMES/setwallpapergroup.sh
 
